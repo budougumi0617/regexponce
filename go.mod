344dafb456f5410eb42ebacbdf17535362c3b491
budougumi0617 <budougumi0617@binc.jp>
2020-08-08 23:29:50 +0900

    :white_check_mark: add test case

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/budougumi0617/regexponce
 
 go 1.14
+
+require golang.org/x/tools v0.0.0-20200807235657-19738be007ce
--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -1,8 +1,22 @@
 package a
 
-func f() {
-	// The pattern can be written in regular expression.
-	var gopher int // want "pattern"
-	print(gopher)  // want "identifier is gopher"
+import (
+	"fmt"
+	"regexp"
+)
+
+var okRegexp = regexp.MustCompile(`^[a-z]+\[[0-9]+\]$`) // OK
+
+func init() {
+	var validID = regexp.MustCompile(`^[a-z]+\[[0-9]+\]$`) // OK
+	fmt.Println(validID.MatchString("adam[23]"))
 }
 
+func f() {
+	// The pattern can be written in regular expression.
+	var validID = regexp.MustCompile(`^[a-z]+\[[0-9]+\]$`) // want "regexp\.MustCompile must be called  only once at initialize"
+	fmt.Println(validID.MatchString("adam[23]"))
+
+	// lint:ignore compile ok
+	validID = regexp.MustCompile(`^[a-z]+\[[0-9]+\]$`) // OK
+}
