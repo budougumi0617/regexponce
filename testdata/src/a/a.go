5eb6179f7b6f76472e9a3729d39a92c5c558e575
budougumi0617 <budougumi0617@binc.jp>
2020-08-08 23:34:53 +0900

    :heart: fix test

--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -14,7 +14,7 @@ func init() {
 
 func f() {
 	// The pattern can be written in regular expression.
-	var validID = regexp.MustCompile(`^[a-z]+\[[0-9]+\]$`) // want "regexp\.MustCompile must be called  only once at initialize"
+	var validID = regexp.MustCompile(`^[a-z]+\[[0-9]+\]$`) // want `regexp\.MustCompile must be called  only once at initialize`
 	fmt.Println(validID.MatchString("adam[23]"))
 
 	// lint:ignore compile ok
