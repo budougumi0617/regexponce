5adb340112a0db7df444e41c640b08631a80989a
budougumi0617 <budougumi0617@binc.jp>
2020-08-12 01:07:05 +0900

    :green_heart: fix test

--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -17,6 +17,6 @@ func f() {
 	var validID = regexp.MustCompile(`^[a-z]+\[[0-9]+\]$`) // want `regexp\.MustCompile must be called  only once at initialize`
 	fmt.Println(validID.MatchString("adam[23]"))
 
-	// lint:ignore compile ok
+	// lint:ignore regexp allowed
 	validID = regexp.MustCompile(`^[a-z]+\[[0-9]+\]$`) // OK
 }
