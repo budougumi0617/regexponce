2340e5bf8e2fa94e8f608317040ac009d0af90aa
budougumi0617 <budougumi0617@binc.jp>
2020-08-14 02:31:26 +0900

    :white_check_mark: update test case

--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -12,6 +12,11 @@ func init() {
 	fmt.Println(validID.MatchString("adam[23]"))
 }
 
+func initfake() {
+	validID, _ := regexp.Compile(`^[a-z]+\[[0-9]+\]$`) // want `regexp.Compile must be called only once at initialize`
+	fmt.Println(validID.MatchString("adam[23]"))
+}
+
 func f() {
 	// The pattern can be written in regular expression.
 	var validID = regexp.MustCompile(`^[a-z]+\[[0-9]+\]$`) // want `regexp.MustCompile must be called only once at initialize`
