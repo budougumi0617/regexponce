fd5d3339b664595e4ce34ab399638b0859f6d817
budougumi0617 <budougumi0617@binc.jp>
2020-08-14 22:35:37 +0900

    :white_check_mark: add test case

--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -18,7 +18,6 @@ func initfake() {
 }
 
 func f() {
-	// The pattern can be written in regular expression.
 	var validID = regexp.MustCompile(`^[a-z]+\[[0-9]+\]$`) // want `regexp.MustCompile must be called only once at initialize`
 	fmt.Println(validID.MatchString("adam[23]"))
 
@@ -26,4 +25,16 @@ func f() {
 	validID = regexp.MustCompile(`^[a-z]+\[[0-9]+\]$`) // OK
 	hoge := regexp.MustCompile
 	hoge(`^[a-z]+\[[0-9]+\]$`) // want `regexp.MustCompile must be called only once at initialize`
+}
+func main() {
+	var validID = regexp.MustCompile(`^[a-z]+\[[0-9]+\]$`) // OK because main function runs only once.
+	fmt.Println(validID.MatchString("adam[23]"))
+
+	for i := 0; i < 10; i++ {
+		validID = regexp.MustCompile(`^[a-z]+\[[0-9]+\]$`) // want `regexp.MustCompile must be called only once at initialize`
+	}
+	x := 10
+	if x < 10 {
+		validID = regexp.MustCompile(`^[a-z]+\[[0-9]+\]$`) // OK
+	}
 }
