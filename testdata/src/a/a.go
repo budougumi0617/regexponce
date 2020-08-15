c4201fd58dc7a2e5a419eecc40a43694f59a5eb1
budougumi0617 <budougumi0617@binc.jp>
2020-08-15 23:40:21 +0900

    :white_check_mark: update test

--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -26,15 +26,23 @@ func f() {
 	hoge := regexp.MustCompile
 	hoge(`^[a-z]+\[[0-9]+\]$`) // want `regexp.MustCompile must be called only once at initialize`
 }
+
 func main() {
 	var validID = regexp.MustCompile(`^[a-z]+\[[0-9]+\]$`) // OK because main function runs only once.
 	fmt.Println(validID.MatchString("adam[23]"))
 
+	x := 10
 	for i := 0; i < 10; i++ {
 		validID = regexp.MustCompile(`^[a-z]+\[[0-9]+\]$`) // want `regexp.MustCompile must be called only once at initialize`
+		if x < 10 {
+			validID = regexp.MustCompile(`^[a-z]+\[[0-9]+\]$`) // want `regexp.MustCompile must be called only once at initialize`
+		}
 	}
-	x := 10
+
 	if x < 10 {
 		validID = regexp.MustCompile(`^[a-z]+\[[0-9]+\]$`) // OK
+		for i := 0; i < 10; i++ {
+			validID = regexp.MustCompile(`^[a-z]+\[[0-9]+\]$`) // want `regexp.MustCompile must be called only once at initialize`
+		}
 	}
 }
